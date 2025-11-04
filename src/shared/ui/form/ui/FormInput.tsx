379f4160356fe7d5a1dc065260316f62e75c44be
Philipp Litzenberger <ph.moshkov@gmail.com>
2025-11-04 18:16:13 +0100

    feat: delete old code. Create development branch to use it for development and separate from main branch

--- src/shared/ui/form/ui/FormInput.tsx
+++ src/shared/ui/form/ui/FormInput.tsx
@@ -1,11 +1,3 @@
-import {
-  FormField,
-  FormItem,
-  FormLabel,
-  FormControl,
-  FormMessage,
-  FormDescription,
-} from "@/shared/shadcn/components/ui/form";
 import { Input } from "@/shared/shadcn/components/ui/input";
 import type { BaseFormProps } from "../types/formTypes";
 import { Controller, type FieldValues } from "react-hook-form";
@@ -45,28 +37,12 @@ export const FormInput = <T extends FieldValues>(props: FormInputProps<T>) => {
             placeholder={placeholder}
             type={type}
           />
-          <FieldDescription className="sr-only">{srOnly}</FieldDescription>
-          {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
-        </Field>
-      )}
-    />
-  );
-  return (
-    <FormField
-      control={control}
-      name={name}
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>{label}</FormLabel>
-          <FormControl>
-            <Input placeholder={placeholder} type={type} {...field} />
-          </FormControl>
-          <FormDescription>
+          <FieldDescription>
             {description ?? description}
             {<span className="sr-only">{srOnly}</span>}
-          </FormDescription>
-          <FormMessage />
-        </FormItem>
+          </FieldDescription>
+          {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+        </Field>
       )}
     />
   );
